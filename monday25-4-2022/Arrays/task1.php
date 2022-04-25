f77d9f809e57831edcd83ff762b983c506ba6c09
Taebah shlouh <119taeba@gmail.com>
2022-04-25 21:34:35 +0300

    php

--- monday25-4-2022/Arrays/task1.php
+++ monday25-4-2022/Arrays/task1.php
@@ -1,12 +1,11 @@
 <?php
+// 1.	Write a script to generate the following paragraph 
 $color=array("red","green","white");
-echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the ".$color[0]."carpet, the ".$color[1]." lawn, the ".$color[2]." house, the leaden sky. The new president and his first lady. - Richard M. Nixon";
+echo "The memory of that scene for me is like a frame of film forever frozen at that moment: the ".$color[0]." carpet, the ".$color[1]." lawn, the ".$color[2]." house, the leaden sky. The new president and his first lady. - Richard M. Nixon<br>";
+
 echo str_repeat("-",50)."<br>";
-$color = array('white', 'green', 'red');
-foreach ($color as $c)
-{
-echo "$c, ";
-}
+
+// 2. Write a PHP script that will display the colors as unordered list :
 sort($color);
 echo "<ul>";
 foreach ($color as $y)
@@ -16,6 +15,7 @@ echo "<li>$y</li>";
 echo "</ul>";
 echo str_repeat("-",50)."<br>";
 
+// 3.Create a PHP script to displays the capital and country name from the above array $cities. Sort the list by the capital of the country. 
 $cities= array( "Italy"=>"Rome", "Luxembourg"=>"Luxembourg", "Belgium"=> "Brussels", "Denmark"=>"Copenhagen", "Finland"=>"Helsinki", "France" => "Paris", "Slovakia"=>"Bratislava", "Slovenia"=>"Ljubljana", "Germany" => "Berlin", "Greece" => "Athens", "Ireland"=>"Dublin", "Netherlands"=>"Amsterdam", "Portugal"=>"Lisbon", "Spain"=>"Madrid" ); 
 
 function display($c){
@@ -36,6 +36,7 @@ function First_element($array){
 echo reset($array)."\n";
 }
 First_element($color);
+
 echo "<br>";
 echo str_repeat("-",50)."<br>";
 // 5.	Write a PHP script that inserts a specific new item in an array in any position
@@ -80,6 +81,7 @@ for ($i=($temp_array_length-7); $i< ($temp_array_length); $i++)
 {
 echo $temp_array[$i].", ";
 }
+//////////////////////
 echo "<br>";
 echo str_repeat("-",50)."<br>";
 // 8.	Write a PHP program to merge the following two arrays. Sample Input: 
@@ -88,9 +90,23 @@ $array1 = array("color" => "red", 2, 4);
 $array2 = array("a", "b", "color" => "green", "shape" => "trapezoid", 4);
 
 $result = array_merge($array1, $array2);
-print_r($result);
-
+foreach ($result as $key => $value) {
+    echo "[$key] => $value <br>";
+}
+////////////////////////
+echo str_repeat("-",50)."<br>";
 // 9.	Write a PHP function to change the following array's and convert all the strings to upper case.Sample Input:
 $colors = array("red","blue", "white","yellow");
+function myfunction($v) 
+{
+$v=strtoupper($v);
+  return $v."<br>";
+}
+print_r(array_map("myfunction",$colors));
+/////////////////////////////////////
+echo "<br>";
+echo str_repeat("-",50)."<br>";
+// 10.	Write a PHP function to change the following array's and convert all the strings to lower case. 
+function 
 
 ?>
