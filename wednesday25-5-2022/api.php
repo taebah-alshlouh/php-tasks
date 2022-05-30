30bbbcaa5b60e6e56773172af97cfc49f55bf296
Taebah shlouh <119taeba@gmail.com>
2022-05-30 17:57:11 +0300

    done

--- wednesday25-5-2022/api.php
+++ wednesday25-5-2022/api.php
@@ -6,24 +6,18 @@ include('connection.php');
     $query = "SELECT * FROM `employees`";
     $result = mysqli_query($conn,$query);
     // $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-   while($row=mysqli_fetch_assoc($result)){
-    $emplyeeInfo['emp_id'] = $row['emp_id'];
-    $emplyeeInfo['emp_name'] = $row['emp_name'];
-    $emplyeeInfo['emp_email'] = $row['emp_email'];
-    $emplyeeInfo['emp_img'] = $row['emp_img'];
-    $emplyeeInfo['emp_salary'] = $row['emp_salary'];
-    $emplyeeInfo['emp_department'] = $row['emp_department'];
-    $emplyeeInfo['emp_address'] = $row['emp_address'];
-    // $response["status"] = "true";
-    // $response["message"] = "emplyeeInfo";
-    // $response["employees"] = $emplyeeInfo;
+   while($row=mysqli_fetch_array($result,MYSQLI_ASSOC)){
 
-    // exit;
+    $emplyeeInfo[]= array(
+    'emp_id'=> $row['emp_id'],'emp_name'=> $row['emp_name'],'emp_email'=> $row['emp_email'],'emp_img'=> $row['emp_img'],'emp_salary'=> $row['emp_salary'],'emp_department'=> $row['emp_department'],
+    'emp_address'=> $row['emp_address'],
+    );
+
+    $response["status"] = "true";
+    $response["message"] = "emplyeeInfo";
+    $response["employees"] = $emplyeeInfo;
    }
    echo json_encode($emplyeeInfo); 
-
-
-
-
+    exit;
 
 ?>
